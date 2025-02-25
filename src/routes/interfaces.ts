605786b68393e1181394061f1635ee2761c9c6db
Omar Sosa <omar.sosa.quimica@gmail.com>
2025-02-25 16:10:51 -0700

    Refactor user controller methods for improved response handling and add UserControllerType interface

---
+++ src/routes/interfaces.ts
@@ -0,0 +1,13 @@
+export type UserControllerType={
+    logIn:Function,
+    signUp:Function,
+    getProfile:Function,
+    uploadPost:Function,
+    postLike:Function,
+    removeLike:Function,
+    getPosts:Function,
+    getFolloweePosts:Function,
+    postComment:Function,
+    deleteComment:Function,
+    getComments:Function,
+}
