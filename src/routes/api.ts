92df69f55034d3502a1f3f88da85d66b941eb21b
Omar Sosa <omar.sosa.quimica@gmail.com>
2025-02-25 16:10:37 -0700

    Add API routes for user authentication and management

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,1 +1,23 @@
-export {Router} from 'express'
+import { Router } from "express";
+import { UserController } from "../controller/userController";
+import { authenticate } from "../middlewares/token_validation";
+import type { UserModel } from "../controller/interfaces";
+
+export const createRouter = ({ UserModel }: { UserModel: UserModel }) => {
+    const apiRouter = Router();
+    const userController = new UserController({ UserModel });
+
+    apiRouter.post("/login", userController.logIn);
+    apiRouter.post("/signup", userController.signUp);
+    apiRouter.get("/profile", userController.getProfile);
+    apiRouter.get("/posts", userController.getPosts);
+    apiRouter.get("/followeePosts", authenticate, userController.getFolloweePosts);
+    apiRouter.post("/uploadpost", authenticate, userController.uploadPost);
+    apiRouter.post("/like", authenticate, userController.postLike);
+    apiRouter.delete("/like", authenticate, userController.removeLike);
+    apiRouter.post("/comment", authenticate, userController.postComment);
+    apiRouter.delete("/comment", authenticate, userController.deleteComment);
+    apiRouter.get("/comments", userController.getComments);
+
+    return apiRouter;
+};
