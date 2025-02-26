af747229a78466b5943ef834ba9dda7acacf25dc
Omar Sosa <omar.sosa.quimica@gmail.com>
2025-02-25 17:30:54 -0700

    Refactor application structure to integrate CORS middleware, create API router, and enhance app initialization with UserModel

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 import express from "express";
+import { corsMiddleware } from "./middlewares/cors";
+import { createRouter } from "./routes/api";
+import type { UserModel } from "./controller/interfaces";
 
+export const createApp = ({UserModel}:{UserModel:UserModel}) => {
+    const app = express()
+    const PORT = process.env.PORT ?? 3000
 
-const app = express()
-const PORT = process.env.PORT ?? 3000
-app.use(express.json())
-app.disable('x-powered-by')
-app.get('/', (req, res)=>{
-    res.status(200).send('hello worldd')
-})
-
-app.post('/login', (req, res)=>{})
-app.post('/register', (req, res)=>{})
-app.post('/logout', (req, res)=>{})
-app.post('/protected', (req, res)=>{}) 
-
-
-app.listen(PORT, ()=>{
-    console.log(`app listening in http://localhost:${PORT}`)
-})
+    app.use(express.json())
+    app.use(corsMiddleware())
+    app.disable('x-powered-by')
+    app.use('/v1', createRouter({UserModel}))
+    app.get('/', (req, res)=>{
+        res.status(200).send('hello worldd')
+    })
+    app.listen(PORT, ()=>{
+        console.log(`app listening in http://localhost:${PORT}`)
+    })
+}
---
+++ src/server/mysql.ts
@@ -0,0 +1,4 @@
+import { AppModel } from "../models/mysql_model";
+import { createApp } from "../index";
+
+createApp({UserModel:AppModel})
