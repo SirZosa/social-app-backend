8ca0f3caacfd981116051e59f61c5cea7ac1bcdc
Omar Sosa <omar.sosa.quimica@gmail.com>
2025-03-20 15:18:54 -0600

    organize routes

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -7,17 +7,19 @@ import type { UserModel } from "../controller/interfaces.js";
 export const createRouter = ({ UserModel }: { UserModel: UserModel }) => {
     const apiRouter = Router();
     const userController = new UserController({ UserModel });
-
+    
     apiRouter.post("/login", userController.logIn);
+    apiRouter.post("/logout", authenticate, userController.logOut);
     apiRouter.post("/signup", userController.signUp);
     apiRouter.get("/profile/:user_id", userController.getProfile);
     apiRouter.get("/posts", partialAuthenticate,userController.getPosts);
-    apiRouter.get("/post/:post_id", partialAuthenticate, userController.getPostById);
     apiRouter.post("/posts", authenticate, userController.uploadPost);
     apiRouter.delete("/posts", authenticate, userController.deletePost);
+    apiRouter.get("/post/:post_id", partialAuthenticate, userController.getPostById);
+    apiRouter.get("/posts/following", authenticate, userController.getFolloweePosts);
+    apiRouter.get("/posts/saved", authenticate, userController.getSavedPosts);
     apiRouter.post("/like", authenticate, userController.postLike);
     apiRouter.delete("/like", authenticate, userController.removeLike);
-    apiRouter.get("/followeePosts", authenticate, userController.getFolloweePosts);
     apiRouter.post("/comment", authenticate, userController.postComment);
     apiRouter.delete("/comment", authenticate, userController.deleteComment);
     apiRouter.get("/comment/:post_id", userController.getComments);
@@ -25,11 +27,8 @@ export const createRouter = ({ UserModel }: { UserModel: UserModel }) => {
     apiRouter.delete("/follow", authenticate, userController.unfollow);
     apiRouter.post("/savepost", authenticate, userController.savePost);
     apiRouter.delete("/savepost", authenticate, userController.unsavePost);
-    apiRouter.get("/savepost", authenticate, userController.getSavedPosts);
     apiRouter.get("/following/:user_id", userController.getFollowing);
     apiRouter.get("/followers/:user_id", userController.getFollowers);
-    apiRouter.get("/user", authenticate, userController.getUserInfo)
-    apiRouter.post("/logout", authenticate, userController.logOut)
-
+    apiRouter.get("/user", authenticate, userController.getUserInfo);
     return apiRouter;
 };
